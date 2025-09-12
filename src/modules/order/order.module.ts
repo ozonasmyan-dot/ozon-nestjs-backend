ebeceb03fbf65142922efca591a7bf701afd9840
ozonasmyan-dot <ozon.asmyan@gmail.com>
2025-09-12 18:49:36 +0400

    feat: save postings via order module

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { SellerApiModule } from '@/api/seller/seller.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, SellerApiModule],
   controllers: [OrderController],
   providers: [OrderService],
 })
