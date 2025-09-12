6561f63a1ff45793a5afd346d23cfcfe21e46469
ozonasmyan-dot <ozon.asmyan@gmail.com>
2025-09-13 01:07:28 +0400

    feat: aggregate orders with transactions

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -4,10 +4,11 @@ import { OrderController } from './order.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { SellerApiModule } from '@/api/seller/seller.module';
 import { OrderRepository } from './order.repository';
+import { TransactionRepository } from '@/modules/transaction/transaction.repository';
 
 @Module({
   imports: [PrismaModule, SellerApiModule],
   controllers: [OrderController],
-  providers: [OrderService, OrderRepository],
+  providers: [OrderService, OrderRepository, TransactionRepository],
 })
 export class OrderModule {}
