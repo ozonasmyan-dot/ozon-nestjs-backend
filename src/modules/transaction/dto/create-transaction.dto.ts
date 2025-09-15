406a38a1a0035338e403e08467b2745e42b0e841
tigranasman <asmyan.t@promail366.com>
2025-09-15 09:34:21 +0400

    init

--- src/modules/transaction/dto/create-transaction.dto.ts
+++ src/modules/transaction/dto/create-transaction.dto.ts
@@ -2,7 +2,7 @@ export class CreateTransactionDto {
   operationId: string;
   name: string;
   date: Date;
-  postingNumber?: string;
-  sku?: string | number;
+  postingNumber: string;
+  sku: string | number;
   price: number;
 }
